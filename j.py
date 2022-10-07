d6910aa8ad808b01c0d17fc9470be7fdd3158d89
Shroin <shroinmandal@gmail.com>
2022-10-07 17:44:37 +0530

    Create j.py

---
+++ j.py
@@ -0,0 +1,18 @@
+from http.client import UNAUTHORIZED
+from re import U 
+from turtle import exitonclick
+from unittest.mock import NonCallableMock 
+from uuid import UUID
+import pyautogui
+import time 
+
+time.sleep(2)
+for i in range(10):
+    pyautogui.press("")
+    pyautogui.press("")
+    pyautogui.press("")
+    pyautogui.press("")
+    pyautogui.press("enter")
+
+exitonclick
+ 
